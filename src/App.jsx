e4605d2042941ddc25c831bcd95d905a5a58cb40
Maaz Mohiuddin <47687440+maazmohiuddin@users.noreply.github.com>
2023-04-09 03:24:24 +0500

    Boiler Plate

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { BrowserRouter } from "react-router-dom"
+import { About, Contact, Expirence, Feedbacks, Hero, Navbar, Tech, Works, StartsCanvas } from './components'
+
+const App = () =>
+{
+  return (
+    <div>DevXonic</div>
+
+
+  )
+  
+
+}
+
+export default App
