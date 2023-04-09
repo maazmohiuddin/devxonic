75e8da6229041e41c46aa91ab3f7a70945604ac2
Maaz Mohiuddin <47687440+maazmohiuddin@users.noreply.github.com>
2023-04-09 22:02:03 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsxFactory: "h",
+    jsxFragment: "Fragment",
+  },
   build: {
     assetsInlineLimit: 0,
     rollupOptions: {
