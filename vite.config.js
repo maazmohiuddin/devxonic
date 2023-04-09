32f595fbe761381d879e30221fbf1a963dbfb527
Maaz Mohiuddin <47687440+maazmohiuddin@users.noreply.github.com>
2023-04-09 22:09:42 +0500

    Deleted Netlify/toml

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    jsxFactory: "h",
-    jsxFragment: "Fragment",
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        mimeTypes: {
+          js: ["application/javascript"],
+        },
+      },
+    },
   },
 });
