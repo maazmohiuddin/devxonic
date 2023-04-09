d606a72511b62b21b65d4bb8608cd2eb06574261
Maaz Mohiuddin <47687440+maazmohiuddin@users.noreply.github.com>
2023-04-09 21:49:54 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        mimeTypes: {
+          js: ["application/javascript"],
+        },
+      },
+    },
+  },
+});
